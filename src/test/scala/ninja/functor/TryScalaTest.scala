fe52d4ea8fd4e794e4a85239a899c560eaca823b
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-02-07 12:27:50 -0300

    feat: add implicit supplier converter

--- src/test/scala/ninja/functor/TryScalaTest.scala
+++ src/test/scala/ninja/functor/TryScalaTest.scala
@@ -1,15 +1,17 @@
 package ninja.functor
 
 import ninja.functor.BaseTest.USER_SUPPLIER
+import ninja.functor.TryScalaTest.supplierToScala
 import org.junit.jupiter.api.Test
 
+import java.util.function.Supplier
 import scala.util.{Failure, Success, Try};
 
 class TryScalaTest {
   @Test
   def test() = {
-    USER_SUPPLIER.map(f => () => f.get())
-      .map { f => Try { f() } }
+    USER_SUPPLIER
+      .map { f => Try(f()) }
       .map { t => t.map(_.getEmail) }
       .map {
         case Failure(error) => s"error: ${error.toString}"
@@ -17,4 +19,8 @@ class TryScalaTest {
       }
       .foreach(println)
   }
+}
+
+object TryScalaTest {
+  implicit def supplierToScala[A](f: Supplier[A]): () => A = () => f.get
 }
