9164b92d8e2c5f2a3817b105ec277671634e43a9
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-02-07 11:26:47 -0300

    feat: NoneTest throws exception

--- src/test/java/ninja/functor/NoneTest.java
+++ src/test/java/ninja/functor/NoneTest.java
@@ -6,13 +6,9 @@ public class NoneTest extends BaseTest {
   @Test
   public void test() {
     for (int i = 0; i < USER_SUPPLIER.length; i++) {
-      try {
-        final User user = USER_SUPPLIER[i].get();
-        final String email = user.getEmail();
-        System.out.println("email: " + email);
-      } catch (Throwable e) {
-        System.out.println("error: " + e);
-      }
+      final User user = USER_SUPPLIER[i].get();
+      final String email = user.getEmail();
+      System.out.println("email: " + email);
     }
   }
 }
